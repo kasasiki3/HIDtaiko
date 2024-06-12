4a274ad8422f0917282bbd80ad82af6691cdb8e4
kasashiki3 <naota383838@gmail.com>
2024-06-12 22:19:45 +0900

    1300 ver1.2 for pc

---
+++ HIDtaiko_connector_ver1.2/sketch_connector1.2pc1300/sketch_connector1.2pc1300.ino
@@ -0,0 +1,147 @@
+/*
+   Copyright [2023] [kasashiki]
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+/*
+このプログラムはβ版です。今使用しているプログラムを別で保存してから使用してください。
+*/
+#include <Keyboard.h>
+int de = 220;
+const int A1pin = A1;
+const int A2pin = A2;
+const int A0pin = A0; 
+const int A3pin = A3; 
+
+
+//(PC)それぞれの数値を変更することで感度を調節ができます。　例 int lefts = 75; 
+int se0 = 35;//左カッ　]
+int se1 = 55;//右ドン  5
+int se2 = 40;//左ドン　
+int se3 = 35;//右カッ　     
+
+//int se0 = 160;//左カッ　]
+//int se1 = 220;//左ドン  5
+//int se2 = 220;//右ドン　#
+//int se3 = 160;//右カッ　  
+
+//(PC)叩いた時に入力されるキーを変更できます。例 left = 'y';
+char left = 'd';        //左カッ
+char middleleft = 'f';  //左ドン
+char middletight = 'j'; //右ドン
+char right = 'k';       //右カッ
+
+/*(PC)Aはどれかのキーが入力されてから、そのキーの次の入力を受け付けない時間です。
+(PC)Bはどれかのキーが入力されてから、4キーすべての入力を受け付けない時間です。
+*/
+char A = 2; //キー単体のdelay
+char B = 7; //キー全体のdelay
+char C = 20;
+char p1 = 20;
+
+
+long int sv1 =  0;
+long int sv2 =  0;
+long int sv3 =  0;
+long int sv0 =  0;
+long int ti1 =  0;
+long int ti2 =  0;
+long int ti3 =  0;
+long int ti0 =  0;
+long int time =  0;
+long int ti = 0;
+bool swswitching = false;
+void setup() {
+  Serial.begin(9600);
+  Keyboard.begin();
+  pinMode(15, INPUT_PULLUP);
+  pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
+  pinMode(5, INPUT_PULLUP);
+  pinMode(6, INPUT_PULLUP);
+  pinMode(7, INPUT_PULLUP);
+  delay(100);
+  if (digitalRead(15) == LOW) {
+  swswitching = true;
+  delay(de); 
+  }
+
+}
+
+void loop() {
+
+  if(swswitching == false){
+  long int a3 = analogRead(A3pin);
+  long int a0 = analogRead(A0pin); 
+  long int a1 = analogRead(A1pin);
+  long int a2 = analogRead(A2pin);
+  time = millis();
+
+  if (a3 - sv3 >= se3 && time - ti3 > A && time- ti > C) {
+  Keyboard.write(left);
+  ti3 = millis();
+  ti = millis();
+  }
+    if (a0 - sv0 >= se0 && time - ti0 > A && time- ti > C) {
+  Keyboard.write(right);
+  ti0 = millis();
+  ti = millis();
+  }
+    if (a1 - sv1 >= se1 && time - ti1 > A && time- ti > B && time - ti0 > p1 && time - ti3 > p1) { 
+  Keyboard.write(middletight);
+  ti1 = millis();
+  ti = millis();
+  }
+    if (a2 - sv2 >= se2 && time - ti2 > A && time- ti > B && time - ti0 > p1 && time - ti3 > p1) {
+  Keyboard.write(middleleft);
+  ti2 = millis();
+  ti = millis();
+  }
+  sv3 = a3;
+  sv0 = a0;
+  sv1 = a1;
+  sv2 = a2;
+    if (digitalRead(2) == LOW) {
+  Keyboard.write(KEY_UP_ARROW);  //上左
+  delay(de);
+  }
+
+  if (digitalRead(3) == LOW) {//上中　 KEY_ESC) KEY_DOWN_ARROW KEY_RETURN
+  Keyboard.write(KEY_RETURN); 
+  delay(de);
+  }
+
+  if (digitalRead(4) == LOW) { //上右
+  Keyboard.write(KEY_F1); 
+  delay(de);
+  }
+
+  if (digitalRead(5) == LOW) { //下右
+  Keyboard.write(KEY_INSERT); 
+  delay(de);
+  }
+    if (digitalRead(6) == LOW) {//下中
+  Keyboard.write(KEY_ESC); 
+  delay(de);
+  }
+    if (digitalRead(7) == LOW) {//下左　 KEY_ESC) KEY_DOWN_ARROW KEY_RETURN
+  Keyboard.write(KEY_DOWN_ARROW); 
+  delay(de);
+  }
+  }
+}
+  
+  
+  
+ 
